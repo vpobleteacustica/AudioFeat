f17889bead1a27b5f4ce1a558b0928206bfc954e
Victor Poblete <vpoblete@Victors-MacBook-Pro.local>
2023-07-01 18:26:53 -0400

    Add audio experiments and spectrograms

--- contenidos/unidad1/sampling/module_functions.py
+++ contenidos/unidad1/sampling/module_functions.py
@@ -332,3 +332,23 @@ class BgrToGrayscale(Module):
 
 def forward(self, image: Tensor) -> Tensor:
         return bgr_to_grayscale(image)
+
+def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+        
+    for c in range(num_channels):
+        axes[c].specgram(waveform[c], Fs=sample_rate)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"Channel {c+1}")
+        if xlim:
+            axes[c].set_xlim(xlim)
+    figure.suptitle(title)
+    plt.xlabel('time (seconds)', fontsize=18)
+    plt.ylabel('frequency (Hz)', fontsize=18)
+    plt.show(block=False)
